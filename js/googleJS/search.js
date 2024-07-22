a944c5380f84d1c7e314a356a2a8818e907ebe71
Molly <9390548+MollyLove@user.noreply.gitee.com>
2024-07-22 21:23:45 +0800

    第6次

---
+++ js/googleJS/search.js
@@ -0,0 +1,30 @@
+// 节点捕获 与 变量
+const moreData=[{googleApp:[
+    {name:'账号' ,imgUrl:'img/img-google/app1.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'搜索' ,imgUrl:'img/img-google/app2.png'},
+    {name:'地图' ,imgUrl:'img/img-google/app3.png'}
+    ]}
+    ,{
+    anthorApp:[
+    {name:'新闻' ,imgUrl:'img/img-google/app4.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Meet' ,imgUrl:'img/img-google/app6.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'},
+    {name:'Gemail' ,imgUrl:'img/img-google/app5.png'}
+    ]},];
